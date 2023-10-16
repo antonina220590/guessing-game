afe88f7f6179363681e27746a96d8869a9845d4a
antonina220590 <tonya2290@gmail.com>
2023-10-16 17:38:44 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,18 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        while (this.max - this.min > 1) {
+            return this.middle = Math.round((this.min + this.max)/2)
+        }
     }
 
     lower() {
-
+        this.max = this.middle
     }
 
     greater() {
+        this.min = this.middle
 
     }
 }
